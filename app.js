2231a229d37427a24d6e448d8613e49cd3cba34e
zachferguson <zach@debian>
2018-12-27 23:20:05 -0500

    blah

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("app.js works");
+let express = require('express');
+let app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  res.send('this is the home page');
+});
+
+app.get('/contact', function(req, res){
+  res.send('this is the contact page');
+});
+
+app.get('/profile/:name', function(req, res){
+  res.render('profile');
+});
